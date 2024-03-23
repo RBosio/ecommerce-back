b7698be49831d1b6efae42728765928f6b5592e0
Fiido <roccobosio@gmail.com>
2024-03-23 08:24:38 -0300

    mail microservice

--- apps/users/src/main.ts
+++ apps/users/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { UsersModule } from './users.module';
+import { RmqService } from '@app/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(UsersModule);
+
+  const rmqService = app.get(RmqService);
+
+  app.connectMicroservice(rmqService.getRmqOptions('USER'));
+  
+  await app.startAllMicroservices();
   await app.listen(null);
 }
 bootstrap();
--- libs/common/src/database/index.ts
+++ libs/common/src/database/index.ts
@@ -1,1 +1,3 @@
 export * from './database.module';
+export * from './base-repository.interface';
+export * from './base-repository';
