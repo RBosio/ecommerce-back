0355970e3cd6d11c985ad7f5423ca71e5b980fce
Fiido <roccobosio@gmail.com>
2024-03-25 05:13:40 -0300

    find user consumer

--- apps/users/src/users.module.ts
+++ apps/users/src/users.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { UsersController } from './users.controller';
+import { UsersController } from './infrastructure/users.controller';
 import {
   DatabaseModule,
   RmqModule,
