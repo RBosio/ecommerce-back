5a3fc5615515260e02daa01fc2f41133c37fcaea
Fiido <roccobosio@gmail.com>
2024-03-25 04:05:18 -0300

    start auth microservice

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
+import { RmqService } from '@app/common';
+import { AUTH_SERVICE } from 'apps/api-gateway/src/constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
+
+  const rmqService = app.get(RmqService);
+  app.connectMicroservice(rmqService.getRmqOptions(AUTH_SERVICE));
+
   await app.listen(null);
+  app.startAllMicroservices();
 }
 bootstrap();
