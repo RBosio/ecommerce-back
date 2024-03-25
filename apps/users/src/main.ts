05d68fce768eea4fda260b263ec623819b0a659f
Fiido <roccobosio@gmail.com>
2024-03-25 04:33:22 -0300

    start user microservice

--- apps/users/src/main.ts
+++ apps/users/src/main.ts
@@ -1,15 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { UsersModule } from './users.module';
 import { RmqService } from '@app/common';
+import { USER_SERVICE } from 'apps/api-gateway/src/constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(UsersModule);
 
   const rmqService = app.get(RmqService);
 
-  app.connectMicroservice(rmqService.getRmqOptions('USER'));
-  
-  await app.startAllMicroservices();
+  app.connectMicroservice(rmqService.getRmqOptions(USER_SERVICE));
+
   await app.listen(null);
+  await app.startAllMicroservices();
 }
 bootstrap();
